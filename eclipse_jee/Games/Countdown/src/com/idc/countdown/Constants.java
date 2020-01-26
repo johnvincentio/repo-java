289873fab2d2da37774884f69e488da0e2892fd0
johnvincentio <john@johnvincent.io>
2020-01-26 00:39:46 -0500

    clean

--- eclipse_jee/Games/Countdown/src/com/idc/countdown/Constants.java
+++
@@ -1,8 +0,0 @@
-
-package com.idc.countdown;
-
-public class Constants {
-	public static final int MAX_NUMBERS = 6;
-	public static final int MAX_ARRAY = MAX_NUMBERS * 2 - 1;
-	private Constants() {}
-}
