e7d4a68bf4ee5c7e0dfaa9ea56129bfe80eff782
johnvincentio <john@johnvincent.io>
2023-09-15 15:52:59 -0400

    added

--- eclipse_jee/mermaid/MakeClasses/src/io/johnvincent/mermaid/App.java
+++ eclipse_jee/mermaid/MakeClasses/src/io/johnvincent/mermaid/App.java
@@ -3,6 +3,14 @@ package io.johnvincent.mermaid;
 import java.io.File;
 import java.util.List;
 
+/*
+ https://stackoverflow.com/questions/61246154/dynamically-loading-and-instantiating-a-class-that-is-not-in-the-classpath
+ 
+ http://www.java2s.com/Tutorial/Java/0125__Reflection/LoadingaClassThatIsNotontheClasspath.htm
+ 
+ 
+ * 
+ */
 public class App {
 	
 	private static final String FOLDER = 
@@ -11,6 +19,17 @@ public class App {
 	"/target/classes/io/johnvincent/gomoku/library/pattern/";
 	private static final String SUFFIX = ".class";
 	
+	private static final String TESTFILE = 
+		"/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target" + 
+	"/classes/io/johnvincent/gomoku/library/pattern/Pattern.class";
+	
+	private static final String AA = 
+		"/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/" + 
+		"/src/main/java/io/johnvincent/gomoku/library/pattern/Pattern.java";
+	
+	private static final String BB = 
+		"/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes/";
+	
     public static void main(String[] args) {
     	List <String> filesList = (new MakeList(new File(FOLDER), SUFFIX).makeList());
         System.out.println("Included files follow:");
@@ -18,5 +37,11 @@ public class App {
             System.out.println(file);
         }
         System.out.println("End of Included files");
+        
+        LoadClasses loadClasses = new LoadClasses(filesList);
+//        loadClasses.doWork(AA);
+        
+        loadClasses.doWork2(BB);
+        
     }
 }
--- eclipse_jee/mermaid/MakeClasses/src/io/johnvincent/mermaid/LoadClasses.java
+++ eclipse_jee/mermaid/MakeClasses/src/io/johnvincent/mermaid/LoadClasses.java
@@ -1,5 +1,8 @@
 package io.johnvincent.mermaid;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.List;
 
 public class LoadClasses {
@@ -12,6 +15,66 @@ public class LoadClasses {
 	}
 	
 	public void doWork(String str) {
-		
+		System.out.println(">>> doWork; file "+ str);
+		try {
+			Class.forName(str);
+		}
+		catch(Exception ex) {
+			System.out.println("Exception; ex "+ex);
+		}
 	}
+
+	// /Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes
+	
+	public void doWork1(String str) {
+		System.out.println(">>> doWork1; file "+ str);
+		String a1 = 
+"file:/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes";
+		
+		try {
+			URL[] urls = new URL[]{new URL(a1)};
+			System.out.println("urls "+urls);
+			ClassLoader cl = new URLClassLoader(urls);
+			String p1 = "io.johnvincent.gomoku.library.pattern.Pattern";
+			cl.loadClass(p1).getDeclaredConstructor().newInstance();
+		}
+		catch(Exception ex) {
+			System.out.println("Exception; ex "+ex);
+		}
+	}
+	
+	public void doWork2(String str) {
+		System.out.println(">>> doWork2; file "+ str);
+		String a1 = 
+"/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes/";
+
+		String p1 = "io.johnvincent.gomoku.library.pattern.Pattern";
+		try {
+			System.out.println("doWork2 - stage 1");
+		    File file = new File(a1);
+		    System.out.println("doWork2 - stage 2");
+
+		    URL url = file.toURI().toURL();
+		    System.out.println("doWork2 - stage 3");
+		    URL[] urls = new URL[] { url };
+		    System.out.println("doWork2 - stage 4");
+
+		    ClassLoader cl = new URLClassLoader(urls);
+		    System.out.println("doWork2 - stage 5");
+
+		    Class cls = cl.loadClass(p1);
+		    System.out.println("doWork2 - stage 100");
+		}
+		catch(Exception ex) {
+			System.out.println("Exception; ex "+ex);
+		}
+	}
+	
 }
+
+/*
+/Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes/io/johnvincent/gomoku/library/pattern 
+*/
+
+
+// /Users/jv/Desktop/MyDevelopment/github/website/gomoku/gomoku-server/gomoku-server/target/classes/
